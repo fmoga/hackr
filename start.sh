1aae977c8ccf03cc0a1abe4dec802379adf413d9
fmoga <fmoga@fmoga-laptop.(none)>
2012-03-13 00:06:35 +0200

    Change file permissions. Add application log file.

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python manage.py runfcgi method=threaded host=0.0.0.0 port=8888 pidfile=`pwd`/django.pid
+python manage.py runfcgi method=threaded host=0.0.0.0 port=8888 pidfile=`pwd`/django.pid outlog=`pwd`/hackr.log
